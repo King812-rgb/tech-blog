e7443a9a1e7565c2c52ffe42aeb4b9ff2e6c61e1
Masaki Nagura <m.nagura12@gmail.com>
2025-03-19 13:25:57 +0900

    the first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+"use client";
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-black text-white">
+      <h1 className="text-5xl font-bold">Hello, World!</h1>
+    </div>
+  );
+}
